19ecc3bdc166a3f08b34ae8789965c016a009603
Daniel Barde <mrbarde2k@gmail.com>
2017-08-16 14:54:04 +0100

    fix webpack

--- src/tasks/WebpackTask.js
+++ src/tasks/WebpackTask.js
@@ -5,11 +5,8 @@ const plumber = require('gulp-plumber');
 
 module.exports = function(options){
     options.callback = options.callback || function() {};
-    return function(){
-        
-        console.log(options);
-
-        gulp.src(options.source)
+    return () => {
+        return gulp.src(options.source)
             .pipe(plumber())
             .pipe(webpackStream(options.config, webpack, options.callback))
             .pipe(gulp.dest(options.destination));
