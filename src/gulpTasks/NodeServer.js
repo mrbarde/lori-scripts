9a87f9a9b1214b08f7ce86def8726b83f6c78b3d
Daniel Barde <mrbarde2k@gmail.com>
2017-08-16 14:04:45 +0100

    add callback to node server

--- src/gulpTasks/NodeServer.js
+++ src/gulpTasks/NodeServer.js
@@ -2,14 +2,18 @@ const {execSync} = require('child_process');
 
 class NodeServer{
 
-    constructor(file){
+    constructor(file, cb){
         this.server = null;
         this.file = file;
         this.command = "node "+this.file;
+        this.cb = cb || null;
     }
 
     start(){
         this.server = execSync(this.command);
+        if(this.cb){
+            this.cb();
+        }
     }
 }
 
