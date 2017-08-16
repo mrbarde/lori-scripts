d5a288b30491c8822c696f54a21610a06da5ccb3
Daniel Barde <mrbarde2k@gmail.com>
2017-08-16 13:54:50 +0100

    add callback to webpack task

--- src/gulpTasks/WebpackTask.js
+++ src/gulpTasks/WebpackTask.js
@@ -3,11 +3,12 @@ const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 const plumber = require('gulp-plumber');
 
-module.exports = function({source, config, destination}){
+module.exports = function({source, config, destination, callback}){
+    callback = callback || function() {};
     return function(){
         return gulp.src(source)
                    .pipe(plumber())
-                   .pipe(webpackStream(config, webpack))
+                   .pipe(webpackStream(config, webpack, callback))
                    .pipe(gulp.dest(destination));
     }
 }
