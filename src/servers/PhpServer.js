b35cad44b4f428752d230d58b9768c0134274cf0
Daniel Barde <mrbarde2k@gmail.com>
2017-08-16 15:37:22 +0100

    fix bug

--- src/servers/PhpServer.js
+++ src/servers/PhpServer.js
@@ -3,19 +3,18 @@ const connect = require('gulp-connect-php');
 class PhpServer{
 
     constructor(options, cb){
-        this.server = null;
+        this.server = new connect;
         this.options = options;
         this.cb = cb || function(){};
     }
 
     start(){
-        this.server = connect.server(this.options, this.cb);
+        this.server.server(this.options, this.cb);
         // close server on process close
         if(this.server){
             if(this.server.hasOwnProperty('closeServer')){
                 process.on('close', this.server.closeServer);
             }
-
         }
     }
 }
