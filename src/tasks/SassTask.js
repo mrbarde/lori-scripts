f81be469b412be9418970fbba7da74aa30296fe4
Daniel Barde <danielbarde@USERs-MacBook-Pro.local>
2018-07-10 10:37:49 +0100

    switch from gulp-ruby-sass to gulp-sass

--- src/tasks/SassTask.js
+++ src/tasks/SassTask.js
@@ -1,17 +1,29 @@
 const gulp = require('gulp');
-const sass = require('gulp-ruby-sass');
+const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const pump = require('pump');
 
 module.exports = function({source, options, file, destination}){
-    return function(){
-        return sass(source, options)
-        .pipe(plumber())
-        .pipe(autoprefixer())
-        .pipe(cssnano())
-        .pipe(rename(file))
-        .pipe(gulp.dest(destination));
-    };
+    var prefixes = options && options.autoprefixer ? options.autoprefixer : {};
+    var task = [gulp.src(source)];
+
+    if (options && typeof options.sourcemaps === true){
+        task = task.concat([
+            sourcemaps.init(),
+            sass().on('error', sass.logError),
+            sourcemaps.write(),
+        ]);
+    }else{ task = task.concat([ sass().on('error', sass.logError) ]); }
+
+    return pump(task.concat([
+        autoprefixer(prefixes),
+        plumber(),
+        cssnano(),
+        rename(file),
+        gulp.dest(destination),
+    ]));
 };
